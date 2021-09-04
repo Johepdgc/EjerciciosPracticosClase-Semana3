113c878771337de1367a0541eb35e17cb7d90106
Johepdgc <88364916+Johepdgc@users.noreply.github.com>
2021-09-03 21:31:57 -0600

    Add files via upload

---
+++ ArrayLista.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayLista
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Que productos se compraran en el supermercado?");
+            var groceries = new List<String>();
+            groceries.Add(Console.ReadLine());
+            groceries.Add(Console.ReadLine());
+            groceries.Add(Console.ReadLine());
+            groceries.Add(Console.ReadLine());
+            groceries.Add(Console.ReadLine());
+
+            Console.WriteLine("La lista de compras es:");
+
+            foreach (var product in groceries)
+                Console.WriteLine(product);
+
+            Console.ReadKey();
+        }
+    }
+}
