e31c7fbbda54c153ebc01211eabc0fc3cf5fa5d1
Johepdgc <88364916+Johepdgc@users.noreply.github.com>
2021-09-03 21:22:34 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Arraywithsort
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Con quienes vives en tu hogar?");
+
+            var family = new string[] { "Jose. Papá", "Melissa. Mamá", "Johep", "Drake. Mascota", "Mai. Mascota", "Perla. Mascota" };
+
+            foreach (var member in family)
+                Console.WriteLine(member);
+
+            Console.ReadKey();
+        }
+    }
+}
